80b1718b6770d8239dabe33ad105ec71f19b8fd6
sgopalswamytamu <sgopalswamy@tamu.edu>
2025-01-11 14:20:16 -0600

    testing commit once again

--- example5/ex_transformPlot.m
+++ example5/ex_transformPlot.m
@@ -18,7 +18,7 @@ a.EdgeColor = [0 0 0];
 a.FaceColor = 'none'
 
 % perform an animated "simulation" - no dynamics, just kinematics
-for x = linspace(0,4*pi,80000)
+for x = linspace(0,4*pi,40000)
     y = sin(x);
     addpoints(h,x,y)
 
@@ -26,8 +26,8 @@ for x = linspace(0,4*pi,80000)
     car = [x - w/2, y - w; x + w/2, y-w; x+w/2, y+w; x-w/2, y+w];
 
     % rotate car according to slope of curve 
-    %car2 = rotate(car'-[x;y], pi/2 - cos(x))' + [x;y]';
-    car2 = rotate(car'-[x;y], 0)' + [x;y]';
+    car2 = rotate(car'-[x;y], pi/2 - cos(x))' + [x;y]';
+    %car2 = rotate(car'-[x;y], 0)' + [x;y]';
 
     %update car image
     a.XData = car2(:,1);
